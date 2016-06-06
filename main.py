9b99a7876981d12cc5f19e2db1913c1720826ff6
Audric D'Hoest <audricd@gmail.com>
2016-06-06 16:23:40 +0200

    v0.2
    added menu. dstatus, act, inpkey and unpkey

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 This requires for Office to be installed. Meant to be used when activation through GUI is not enough
 Developed on Windows 2012r2 Office 2013 with Python 3.5.1
 first release, shows outputs dstatus
-TODO: inpkey, unpkey, setkms, and the rest of the functions
+TODO: setkms, setport and the rest of the functions
 http://github.com/audricd
 """
 
@@ -10,8 +10,8 @@ from tkinter import filedialog
 from tkinter import messagebox
 import subprocess
 import os
-
 import sys
+
 if sys.version_info[0] < 3:
     import Tkinter as Tk
 else:
@@ -33,8 +33,42 @@ messagebox.showinfo("Welcome to Microsoft Office Activation Assistant v0.1",
                     "\n It is mandatory to be running this from an administrator account")
 
 ruta = browse_file()
-print("Launching OSPP.vbs status check from " + ruta)
-subprocess.check_call(['cscript', 'ospp.vbs', '/dstatus'], cwd=ruta[:-8])
+
+
+def dstatus():
+    print("Launching OSPP.vbs status check from " + ruta)
+    subprocess.check_call(['cscript', 'ospp.vbs', '/dstatus'], cwd=ruta[:-8])
+
+
+def act():
+    print("Launching OSPP.vbs activation attempt from " + ruta)
+    subprocess.check_call(['cscript', 'ospp.vbs', '/act'], cwd=ruta[:-8])
+
+def inpkey():
+    keytoinstall = input("Type in your product key:")
+    subprocess.check_call(['cscript', 'ospp.vbs', '/inpkey:' + keytoinstall], cwd=ruta[:-8])
+
+def unpkey():
+    keytoremove = input("Type in the last segment of the key you want to remove:")
+    subprocess.check_call(['cscript', 'ospp.vbs', '/unpkey:' + keytoremove], cwd=ruta[:-8])
+
+print("Python Microsoft Windows Office Activation Assistant Menu:"
+      "\n 1. check current activation status"
+      "\n 2. launch activation status"
+      "\n 3. install key"
+      "\n 4. remove key")
+
+menuchoice = input("Type in the menu option:")
+if menuchoice == "1":
+    dstatus(),
+elif menuchoice == "2":
+    act(),
+elif menuchoice == "3":
+    inpkey(),
+elif menuchoice == "4":
+    unpkey(),
+else:
+    print("invalid choice")
 
 os.system("pause")
 
